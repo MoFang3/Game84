2ef88a62670c72b0f0ee6f6710fd9d6b6f9cdd6a
MoFang3 <dobzhanskiyigor100@gmail.com>
2022-06-19 22:33:58 +0300

    first game

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Game84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game84 a = new Game84();
+
+            for (int i = 1; i <= 84; i++)
+                Console.WriteLine("Game: " + a._84[i-1]);
+        }
+    }
+
+    class Game84
+    {
+        public int[] _84 = new int[84];
+
+        public Game84()
+        {
+            for (int i = 1; i <= 84; i++)
+            {
+                _84[i - 1] = i;
+            }
+        }
+    }
+}
